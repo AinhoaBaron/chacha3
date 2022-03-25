a650471ab5a979c0e543ac7a3988e8346cecb90e
AinhoaBaron <93262452+AinhoaBaron@users.noreply.github.com>
2022-03-25 14:26:08 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
 input.onButtonPressed(Button.A, function () {
-    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 0)
-    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 0)
+    for (let index = 0; index < 2; index++) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 40)
+        basic.pause(1500)
+        DFRobotMaqueenPlus.mototStop(Motors.ALL)
+        basic.pause(500)
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 40)
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CCW, 40)
+        basic.pause(545)
+        DFRobotMaqueenPlus.mototStop(Motors.ALL)
+        basic.pause(500)
+    }
 })
 DFRobotMaqueenPlus.I2CInit()
-basic.forever(function () {
-	
-})
